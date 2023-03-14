45d69394d16064767c8dee18b8f410cc7650cc30
andre <andrevzray@gmail.com>
2023-03-14 11:41:56 -0300

    front end almost done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+
+import { AuthProvider } from './hooks/auth';
+
+import { Routes } from './Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 )
