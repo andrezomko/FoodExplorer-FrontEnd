f31a0ea3e337dd89b3d7912b0b50a681220c6e66
andre <andrevzray@gmail.com>
2023-03-28 09:17:50 -0300

    vite problems

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/opt/build/repo/node_modules/styled-components/dist/styled-components.min.js'
+      ]
+    }
+  }
 })
